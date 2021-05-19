fba070171ea3421e1eacdf067c93467bb9b4cf03
Tatiana-C-H <taty_ana06@hotmail.com>
2021-05-19 08:52:12 -0500

    Examen p2

--- ej1.c
+++ ej1.c
@@ -1,2 +1,36 @@
-// ejercicio 1 puntero hacia un char (var p)
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <pthread.h>
 
+char x='x';
+char* p=&x;
+
+char r;
+
+char y='y';
+char* puntero=&y;
+
+
+int main()
+{
+	//printf(" Contenido de p: %s \n", *p);
+	printf(" Contenido de p: %d \n", *p);
+	//printf(" Direccion de p: %p \n", *puntero);
+
+	*p+=1;
+	//printf("\n Contenido de p: %s \n", *p);
+	printf("\n Contenido de p: %d \n", *p);
+
+	r = *p+2;
+	//printf("\n Contenido de p: %s \n", *p);
+	printf("\n Contenido de p: %d \n", r);
+
+	//printf("\n Contenido de p: %s \n", *puntero);
+	printf("\n Contenido de p: %d \n", *puntero);
+
+	return 0;
+}
